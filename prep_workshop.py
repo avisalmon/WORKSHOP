89fabb71d77ab7c4a0fb6f3f724274af6a394ea6
Salmon <avi.salmon@intel.com>
2023-10-24 15:23:59 +0300

    added thonny support

--- prep_workshop.py
+++ prep_workshop.py
@@ -17,6 +17,57 @@ import subprocess
 import sys
 import socket
 import os
+import win32com.client
+import platform
+import serial.tools.list_ports
+import re
+import serial
+import time
+
+def find_your_esp32():
+    wmi = win32com.client.GetObject("winmgmts:")
+    drivers = wmi.InstancesOf("Win32_SystemDriver")
+
+    list_of_drivers = []
+
+    CP210 = False
+
+    for driver in drivers:
+        list_of_drivers.append(driver.description)
+        if "CP210" in driver.description:
+            print("Driver CP210X installed. Great!")
+            CP210 = True
+
+    if not CP210:
+        driverexe = r'CP210xVCPInstaller_x64.exe'
+
+        # Run the executable and wait until it's done
+        subprocess.run(driverexe, shell=True, check=True)
+
+    #check if ESP32 is connected
+    com_number = 0
+    while com_number == 0:
+        ports = serial.tools.list_ports.comports()
+            
+        pattern = r'COM(\d+)'
+
+        if not ports:
+            print("No COM ports found.")
+        else:
+            #print("Available COM ports:")
+            for port in ports:
+                #print(f"- {port.device}: {port.description}")
+                if "CP210" in port.description:
+                    match = re.search(pattern, port.description)
+                    if match:
+                        com_number = int(match.group(1))
+
+        if com_number:
+            print(f'your ESP32 is connected to COM{com_number}')
+        else:
+            input('\nPlease connect the ESP32 device, than press any key...')
+
+        return com_number
 
 def main():
 
@@ -39,7 +90,7 @@ def main():
     # Check if libraries are installed:
     # Checking for needed libraries. 
 
-    library_name = ['pygetwindow', 'psutil', 'shutil', 'GitPython']
+    library_name = ['pygetwindow', 'psutil', 'shutil', 'GitPython', 'esptool', 'adafruit-ampy']
 
     for library in library_name:
         try:
@@ -62,7 +113,7 @@ def main():
     # Kill all Quartus and unneeded windows
     import psutil
 
-    process_names = ['quartus', 'calc'] 
+    process_names = ['quartus', 'calc', 'thonny'] 
 
     # Find running processes matching the process name
     for process_name in process_names:
@@ -73,7 +124,7 @@ def main():
 
     # Check if there is any Latest directory. If not that means that it's a fresh PC
     # Need to create Latest folder and icon on the desktop. 
-    
+
     directories = ['FPGA_design_store', 'upython-esp32', 'VGAstarter_DE10_lite', 'micropython', 'fab_exp', 'single_button']
 
     directories_plus = directories + ['WORKSHOP']
@@ -105,9 +156,9 @@ def main():
     desktop = os.path.join(os.path.expanduser("~"), "Desktop")
 
     import shutil
-     
+
     # Remove the existing WORKSHOP directory. 
-    
+
 
     directory_path = "C:/WORKSHOP/"
 
@@ -115,11 +166,11 @@ def main():
         print(f"The directory '{directory_path}' exists.")
         shutil.rmtree(directory_path, ignore_errors=True)
         print(f"The directory '{directory_path}' and its contents have been deleted.")
-    
+
     os.mkdir('C:/WORKSHOP')
 
     # Copy from Lates to a new WORKSHOP directory for the student
-      
+
     destination_directory = "C:/WORKSHOP"
 
     def ignore_git_files(directory, filenames):
@@ -136,14 +187,62 @@ def main():
 
 
     quartus_executable_options = ['C:/intelFPGA_lite/17.0/quartus/bin64/quartus.exe',
-                                  'C:/intelFPGA_lite/17.1/quartus/bin64/quartus.exe',
-                                  'C:/intelFPGA_lite/21.1/quartus/bin64/quartus.exe'
+                                'C:/intelFPGA_lite/17.1/quartus/bin64/quartus.exe',
+                                'C:/intelFPGA_lite/21.1/quartus/bin64/quartus.exe'
                                 ]
-    
+
     for location in quartus_executable_options:
         if os.path.exists(location):
-            subprocess.Popen([location])
+            #subprocess.Popen([location])
+            print('now opened Quartus')
             break
+
+    try:
+        #get Thonny path. 
+        home_directory = os.path.expanduser("~")
+        print(f'Home dir: {home_directory}')
+        thonny_path = os.path.join(home_directory, "AppData\Local\Programs\Thonny\\thonny.exe")
+        print(f'Thonny location: {thonny_path}')
+
+        #setup the comport to Thonny before opening it: 
+        com_num = find_your_esp32()
+        
+        # lets burn FW.
+        port = 'COM' + str(com_num)
+        def check_micropython(port):
+            with serial.Serial(port, 115200, timeout=1) as ser:
+                ser.write(b'\r\n')
+                time.sleep(2)
+                response = ser.read_all().decode()
+                print(response)
+                return ">>>" in response
+        
+        if check_micropython(port):
+            print(f"MicroPython detected on {port}")
+        else:
+            print(f"No MicroPython prompt detected on {port}")
+
+            # Burn MicroPython 
+            cmd = f'esptool\esptool.py --chip esp32 --port {port} --baud 460800 write_flash -z 0x1000 ESP32_GENERIC-20230426-v1.20.0.bin'
+            input(f'*********\n\n\nGet Ready! We will now burn the ESP32 to have Micropython\n\nhold the boot button right to the USB port and press enter\n\n*****************')
+            subprocess.run(cmd, shell=True, check=True)
+
+        # process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # stdout, stderr = process.communicate()
+
+        # if process.returncode == 0:
+        #     print("Flashing successful!")
+        #     print(stdout.decode())
+        # else:
+        #     print("Flashing failed!")
+        #     print(stderr.decode())
+
+        subprocess.Popen(thonny_path)
+        print(thonny_path)
+        print('tried to open it')
+        print(f'The ESP32 is on COM{find_your_esp32()}')
+    except subprocess.CalledProcessError:
+        print('Didnt find Thonny')
 
     # pull new github updates
     print('Pulling:' + str(directories_plus))
@@ -163,3 +262,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
